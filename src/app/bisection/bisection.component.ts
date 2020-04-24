9636d138ff5ab80eff539e6e4c953b38ced54b3b
ZakaTiwana <zakatiwana@gmail.com>
2020-04-24 21:58:16 +0500

    mathjs added

--- src/app/bisection/bisection.component.ts
+++ src/app/bisection/bisection.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder,FormGroup } from '@angular/forms';
+import * as math from 'mathjs';
 
 @Component({
   selector: 'app-bisection',
@@ -8,6 +9,9 @@ import { FormBuilder,FormGroup } from '@angular/forms';
 })
 export class BisectionComponent implements OnInit {
   isSubmited = false;
+  ans:Number;
+  f:any;
+
   biFrom:FormGroup ;  
   constructor(private fb:FormBuilder) { }
 
@@ -21,6 +25,17 @@ export class BisectionComponent implements OnInit {
 
   }
 
-  eval(expression:string){
+  eval(){
+    if(this.biFrom.invalid) return;
+    this.isSubmited = true;
+    const parser = math.parser();
+    parser.evaluate('f(x) = '+this.biFrom.value.equation);
+    this.f = parser.get('f');
+    parser.clear();
+    console.log(this.f(2));
+  }
+
+  bisectionMethod(a:number,b:number,p:number,n:number){
+
   }
 }
